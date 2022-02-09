0ee0e3b05e36d62e60e436f9295bc8412844a0e5
Raphael Thurnherr <raphael.thurnherr1990@gmail.com>
2022-02-09 14:46:17 +0100

    fix typo

--- telegram/messages.go
+++ telegram/messages.go
@@ -2,12 +2,12 @@ package telegram
 
 const menuInfoMsg = "Select the projects that you want to follow. You will receive notifications about new governance proposals once they enter the voting period."
 const chainStatisticHeaderMsg = `
-      Chain ┆ Subscribers
- -----------┼-------------`
+      Chain ┆ Subscriptions
+ -----------┼---------------`
 const chainStatisticRowMsg = `
 %11s ┆ %6d`
 const chainStatisticFooterMsg = `
- -----------┼-------------
+ -----------┼---------------
   %s ┆ %6d`
 const userStatisticMsg = `
        Time ┆   Users (change)
