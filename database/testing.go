030aec598aa4cb642e34e8a99d681fb1aa338d06
Raphael Thurnherr <raphael.thurnherr1990@gmail.com>
2022-02-11 21:10:15 +0100

    add some testing

--- database/testing.go
+++ database/testing.go
@@ -1,9 +1,6 @@
 package database
 
-import (
-	"github.com/shifty11/cosmos-gov/ent/chain"
-	"github.com/shifty11/cosmos-gov/ent/proposal"
-)
+import "github.com/shifty11/cosmos-gov/ent/proposal"
 
 func DropChains() {
 	client, ctx := connect()
@@ -16,9 +13,10 @@ func DropProposals() {
 	client, ctx := connect()
 	client.Proposal.
 		Delete().
-		Where(proposal.And(
-			proposal.HasChainWith(chain.NameEQ("osmosis")),
-			proposal.ProposalIDEQ(144),
-		)).
+		//Where(proposal.And(
+		//	proposal.HasChainWith(chain.NameEQ("osmosis")),
+		//	proposal.ProposalIDEQ(144),
+		//)).
+		Where(proposal.StatusEQ(proposal.StatusPROPOSAL_STATUS_VOTING_PERIOD)).
 		ExecX(ctx)
 }
