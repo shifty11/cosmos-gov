fc647211c01793be9a856634afbf36b5c81f829b
Raphael Thurnherr <raphael.thurnherr1990@gmail.com>
2022-04-04 15:30:40 +0200

    generate dart grpc code

--- protoc.sh
+++ protoc.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-protoc -I=api/cosmos-gov-grpc/ --go_out=api/cosmos-gov-grpc/ --go-grpc_out=api/cosmos-gov-grpc/ --dart_out=api/cosmos-gov-grpc/dart/ api/cosmos-gov-grpc/*.proto
+protoc -I=api/cosmos-gov-grpc/ --go_out=api/cosmos-gov-grpc/ --go-grpc_out=api/cosmos-gov-grpc/ --dart_out=grpc:api/cosmos-gov-grpc/dart/ api/cosmos-gov-grpc/*.proto
