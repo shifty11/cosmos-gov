476cf8fb6cefeefc2d2901bc2fd5f97e6efd9c2a
Raphael Thurnherr <raphael.thurnherr1990@gmail.com>
2022-04-19 16:23:59 +0200

    add timestamp.pb.dart to protobuf

--- protoc.sh
+++ protoc.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 protoc -I=api/grpc/protobuf/ --go_out=api/grpc/protobuf --go-grpc_out=api/grpc/protobuf --dart_out=grpc:api/grpc/protobuf/dart/ api/grpc/protobuf/*.proto
+protoc -I=api/grpc/protobuf/ --dart_out=grpc:api/grpc/protobuf/dart/ google/protobuf/timestamp.proto
